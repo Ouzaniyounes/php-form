24db60c4307a69df95623977f4523f7c0591cd6c
Ouzaniyounes <105242342+Ouzaniyounes@users.noreply.github.com>
2024-03-22 09:53:11 +0100

    Add files via upload

--- createaccount.php
+++ createaccount.php
@@ -17,19 +17,20 @@
         session_start();
         if(isset($_POST["create"])) {
             if(isset($_POST["user"]) && isset($_POST["pass"])) {  // Corrected syntax here
+                $username = $_POST['user'];
+                $password = $_POST['pass']; 
+                $_SESSION["username"] =  $username;
+                $_SESSION["password"] = $password ;
+                setcookie("user","$username",time()+3600);
+                setcookie("password","$password",time()+3600);
                 if(isset($_SESSION["username"]) && isset($_SESSION["password"])) {
-                    array_push($_SESSION["username"],$_POST["user"]);
-                    array_push($_SESSION["password"],$_POST["pass"]);
-                    echo" your account created"."<br>";
-                    echo "<a href='loginn.php'> login</a>";
-                } else {
-                    $username = $_POST['user'];
-                    $password = $_POST['pass']; 
-                    $_SESSION["username"] = array($username) ;
-                    $_SESSION["password"] = array($$password) ;
                     echo" your account created"."<br>";
                     echo "<a href='loginn.php'> login</a>";
                 }
+                if(isset($_cookie["username"]) && isset($_cookie["password"])) {
+                    
+                }
+                session.destroy();
                 
                 }
 
